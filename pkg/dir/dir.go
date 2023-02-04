3678cd82c9f2a718412ae0e26f00e02186196c58
Stephan Lukits <stephan.lukits@gmail.com>
2023-02-04 14:27:48 +0200

    add: dir-package for extended file system features
    Firstly features will be added to determine if a GINI executable is
    executed inside a gini-repository.

---
+++ pkg/dir/dir.go
@@ -0,0 +1,26 @@
+/*
+Copyright 2022 - present Stephan Lukits. All rights reserved.
+Use of this source code is governed by the GNU GPLv3 that can
+be found in the LICENSE file.
+
+This file is part of GINI.
+
+GINI is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+GINI is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GINI. If not, see <https://www.gnu.org/licenses/#GPL>.
+*/
+
+/*
+Package dir provides features around the file system which exceed
+provided features by the os-package.
+*/
+package dir

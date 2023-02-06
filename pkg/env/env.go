bfcd3432517f5d82dd5cb9124cd2a95d1de014ca
Stephan Lukits <stephan.lukits@gmail.com>
2023-02-06 20:27:48 +0200

    refac: env: to achieve 100% test coverage
    To do so two properties were added FatalHandler and OS.  FatalHandler
    centralizes the error handlings which should terminate the program.  It
    defaults to a panic and makes it mockable for tests.  OS is an
    implementation of the OSer providing the os.* functionality needed by
    Env.  This makes the used os.* functions mockable and easy to make them
    fail.

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -28,6 +28,7 @@ package env
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
@@ -47,6 +48,17 @@ type Env struct {
 	// caching directory etc.  E.g. for testing it is handy to set Root
 	// to a temporary directory.
 	Root string
+
+	// FatalHandler is used if an Env-instance is unable to operate on
+	// the system in the intended way which by an unset FatalHandler
+	// leads to a panic.
+	FatalHandler Fataler
+
+	// OS provides the os.* functions which may fail and are needed by
+	// an Env-instance.  OS defaults to an instance of an internal type
+	// mapping OSer methods to the corresponding os.* calls.  This
+	// indirection allows to easily mock up error situations.
+	OS OSer
 
 	wd      string
 	conf    string
@@ -70,6 +82,16 @@ func (e *Env) lock() {
 	e.mutex.Lock()
 }
 
+// os returns given Env e's os'er initializing it to its default if
+// unset.  NOTE os expects e to be locked, i.e. there may be race
+// conditions if not.
+func (e *Env) os() OSer {
+	if e.OS == nil {
+		e.OS = &oser{}
+	}
+	return e.OS
+}
+
 // IsUser returns true if given Env e's Home directory is the user's
 // home directory; false otherwise.
 func (e *Env) IsUser() bool {
@@ -77,9 +99,9 @@ func (e *Env) IsUser() bool {
 		return false
 	}
 	home := e.Home()
-	user, err := os.UserHomeDir()
+	user, err := e.os().UserHomeDir()
 	if err != nil {
-		panic(fmt.Sprintf("gini: Env: no home directory: %v", err))
+		e.fatal("gini: Env: no home directory: %w", err)
 	}
 	return home == user
 }
@@ -99,13 +121,21 @@ func (e *Env) Home() string {
 	e.lock()
 	defer e.mutex.Unlock()
 	if e.Root == "" {
-		home, err := os.UserHomeDir()
+		home, err := e.os().UserHomeDir()
 		if err != nil {
-			panic(fmt.Sprintf("gini: Env: no home directory: %v", err))
+			e.fatal("gini: Env: no home directory: %w", err)
 		}
 		e.Root = home
 	}
 	return e.Root
+}
+
+func (e *Env) fatal(msg string, err error) {
+	if e.FatalHandler == nil {
+		panic(fmt.Errorf(msg, err))
+	}
+	e.FatalHandler.Fatal(fmt.Errorf(msg, err))
+	panic("env: handling fatal: expected execution to stop")
 }
 
 // WD returns the current working directory. WD panics if the working
@@ -115,9 +145,9 @@ func (e *Env) WD() string {
 	e.lock()
 	defer e.mutex.Unlock()
 	if e.wd == "" {
-		wd, err := os.Getwd()
+		wd, err := e.os().Getwd()
 		if err != nil {
-			panic(fmt.Sprintf("gini: Env: no working directory %v", err))
+			e.fatal("gini: Env: no working directory %w", err)
 		}
 		e.wd = wd
 	}
@@ -127,16 +157,16 @@ func (e *Env) WD() string {
 // Conf returns the user config directory.
 func (e *Env) Conf() string {
 	e.lock()
-	defer e.mutex.Unlock()
 	if e.conf == "" {
-		conf, err := os.UserConfigDir()
+		conf, err := e.os().UserConfigDir()
 		if err != nil {
 			e.mutex.Unlock()
 			conf = e.Home()
 			e.lock()
 		}
-		e.conf = filepath.Join(conf, "gini")
+		e.conf = filepath.Join(conf, "gini/config")
 	}
+	e.mutex.Unlock()
 	return e.conf
 }
 
@@ -146,17 +176,41 @@ func (e *Env) Logging() string {
 	defer e.mutex.Unlock()
 	if e.logging == "" {
 		e.mutex.Unlock()
-		e.logging = filepath.Join(e.Conf(), "logs")
+		logging := filepath.Join(e.Conf(), "logs")
 		e.lock()
+		e.logging = logging
 	}
 	return e.logging
 }
 
-// MkLogging creates the logging directory and panics if it can't be
-// created.
-func (e *Env) MkLogging() {
-	if err := os.MkdirAll(e.Logging(), 0700); err != nil {
-		panic(fmt.Sprintf(
-			"gini: pkg: env: mk-logging: can't create dir: %v", err))
-	}
+// MkLogging creates the logging directory and errors if MkdirAll errors.
+func (e *Env) MkLogging() error {
+	return e.os().MkdirAll(e.Logging(), 0700)
+}
+
+// Fataler defines the interface for dealing with situation when an
+// environment Env-instance can not operate in the intended way on the
+// system.
+type Fataler interface {
+	Fatal(...interface{})
+}
+
+// Oser defines the interface of os-operations which may fail and are
+// needed by an environment Env-instance to provide its features.
+type OSer interface {
+	UserHomeDir() (string, error)
+	Getwd() (string, error)
+	UserConfigDir() (string, error)
+	MkdirAll(path string, _ fs.FileMode) error
+}
+
+type oser struct{}
+
+func (oser) UserHomeDir() (string, error) { return os.UserHomeDir() }
+func (oser) Getwd() (string, error)       { return os.Getwd() }
+func (oser) UserConfigDir() (string, error) {
+	return os.UserConfigDir()
+}
+func (oser) MkdirAll(path string, fm fs.FileMode) error {
+	return os.MkdirAll(path, fm)
 }
--- pkg/env/env_test.go
+++ pkg/env/env_test.go
@@ -27,7 +27,12 @@ making concurrency save parallel testing easier.
 package env
 
 import (
+	"errors"
+	"fmt"
+	"io/fs"
 	"os"
+	"strings"
+	"sync"
 	"testing"
 
 	. "github.com/slukits/gounit"
@@ -37,12 +42,92 @@ type env struct{ Suite }
 
 func (s *env) SetUp(t *T) { t.Parallel() }
 
+func (s *env) Creates_only_one_initial_mutex(t *T) {
+	// NOTE this is an exceptional white-box test
+	// NOTE this test may block all other tests from initializing their
+	// environment but it doesn't cause any problems.
+	env, got, continue_ := &Env{}, "", make(chan struct{})
+	initMutex.Lock()
+	go func(e *Env) {
+		continue_ <- struct{}{}
+		e.Home()
+		got = fmt.Sprintf("%T::%[1]v", e.mutex)
+		close(continue_)
+	}(env)
+	<-continue_
+	env.mutex = &sync.Mutex{}
+	initMutex.Unlock()
+	<-continue_
+	t.Eq(fmt.Sprintf("%T::%[1]v", env.mutex), got)
+}
+
 func (s *env) Home_dir_defaults_to_user_s_home_directory(t *T) {
 	user, err := os.UserHomeDir()
 	t.FatalOn(err)
 	env := &Env{}
 	t.Eq(user, env.Home())
 	t.True(env.IsUser())
+}
+
+func (s *env) Panics_if_no_home_dir_and_no_fatal_handler(t *T) {
+	os := &oserFX{userHomeDir: func() (string, error) {
+		return "", errors.New("err: home-dir mock")
+	}}
+	env := &Env{OS: os}
+	t.Panics(func() { env.Home() })
+}
+
+func (s *env) Panics_if_fatal_handler_isnt_stopping_execution(t *T) {
+	os := &oserFX{userHomeDir: func() (string, error) {
+		return "", errors.New("err: home-dir mock")
+	}}
+	fatal := &fatalerMock{fatal: func(i ...interface{}) {}}
+	env := &Env{OS: os, FatalHandler: fatal}
+	t.Panics(func() { env.Home() })
+}
+
+func (s *env) Fatales_if_no_home_dir_and_fatal_handler(t *T) {
+	ftlExp := "home-dir fatal mock"
+	os := &oserFX{userHomeDir: func() (string, error) {
+		return "", errors.New("err: home-dir mock")
+	}}
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Fatal("fatal wasn't called")
+		}
+		t.Eq(r, ftlExp)
+	}()
+	fatal := &fatalerMock{fatal: func(i ...interface{}) {
+		panic(ftlExp)
+	}}
+	env := &Env{OS: os, FatalHandler: fatal}
+	env.Home()
+}
+
+func (s *env) Is_not_user_env_if_nil(t *T) {
+	t.Not.True((*Env)(nil).IsUser())
+}
+
+func (s *env) Fatal_if_is_user_cant_get_home(t *T) {
+	first := true
+	os := &oserFX{userHomeDir: func() (string, error) {
+		if first {
+			first = false
+			home, err := os.UserHomeDir()
+			t.FatalOn(err)
+			return home, nil
+		}
+		return "", errors.New("err: home-dir mock")
+	}}
+	ftl, rcv := fatalMockRecover(t, "is-user home-dir fatal mock")
+	defer rcv()
+	env := &Env{OS: os, FatalHandler: ftl}
+	env.IsUser()
+}
+
+func (s *env) Is_not_associated_with_tmp_if_nil(t *T) {
+	t.Not.True((*Env)(nil).IsTemp())
 }
 
 func (s *env) Is_not_associated_with_an_temp_dir_by_default(t *T) {
@@ -54,7 +139,137 @@ func (s *env) Is_associated_with_an_temp_dir_if_root_set_to_one(t *T) {
 	t.True(env.IsTemp())
 }
 
+func (s *env) Fatal_if_no_working_dir_and_fatal_handler(t *T) {
+	os := &oserFX{getwd: func() (string, error) {
+		return "", errors.New("err: working-dir mock")
+	}}
+	ftl, rcv := fatalMockRecover(t, "working-dir fatal mock")
+	defer rcv()
+	env := &Env{OS: os, FatalHandler: ftl}
+	env.WD()
+}
+
+func (s *env) Working_directory_is_os_working_directory(t *T) {
+	wd, err := os.Getwd()
+	t.FatalOn(err)
+	t.Eq(wd, (&Env{}).WD())
+}
+
+func (s *env) Fatal_if_config_dir_cant_be_determined(t *T) {
+	os := &oserFX{
+		userConfigDir: func() (string, error) {
+			return "", errors.New("err: config-dir mock")
+		},
+		userHomeDir: func() (string, error) {
+			return "", errors.New("err: working-dir mock")
+		},
+	}
+	ftl, rcv := fatalMockRecover(t, "config-dir fatal mock")
+	defer rcv()
+	env := &Env{OS: os, FatalHandler: ftl}
+	env.Conf()
+}
+
+func (e *env) Config_dir_is_in_home_dir_if_no_os_config(t *T) {
+	os := &oserFX{userConfigDir: func() (string, error) {
+		return "", errors.New("err: config-dir mock")
+	}}
+	env := &Env{OS: os}
+	t.True(strings.HasPrefix(env.Conf(), env.Home()))
+}
+
+func (e *env) Config_dir_is_in_os_s_config_dir_by_default(t *T) {
+	cnf, err := os.UserConfigDir()
+	t.FatalOn(err)
+	t.True(strings.HasPrefix((&Env{}).Conf(), cnf))
+}
+
+func (e *env) Logging_dir_is_in_config_dir(t *T) {
+	env := &Env{}
+	t.True(strings.HasPrefix(env.Logging(), env.Conf()))
+}
+
+func (e *env) Home_is_set_to_root(t *T) {
+	env := &Env{Root: t.FS().Tmp().Path()}
+	t.Eq(env.Root, env.Home())
+}
+
+func (e *env) Creates_logging_directory(t *T) {
+	// this initialization makes the temporary directory the parent of
+	// the logging directory, hence it will be cleaned up
+	env := &Env{
+		Root: t.FS().Tmp().Path(),
+		OS: &oserFX{userConfigDir: func() (string, error) {
+			return "", errors.New("err: config-dir mock")
+		}},
+	}
+	t.FatalIfNot(t.True(strings.HasPrefix(env.Logging(), env.Root)))
+	env.OS = nil
+	_, err := os.Stat(env.Logging())
+	t.True(err != nil)
+	t.FatalOn(env.MkLogging())
+	_, err = os.Stat(env.Logging())
+	t.True(err == nil)
+}
+
+func fatalMockRecover(t *T, exp string) (Fataler, func()) {
+
+	// fatalerMock must panic to end the execution of the function
+	// calling fatal
+	return &fatalerMock{func(i ...interface{}) { panic(exp) }},
+
+		// since fataler should panic that panic needs to be recovered
+		// to continue with the tests.
+		func() {
+			r := recover()
+			if r == nil {
+				t.Fatal("fatal wasn't called")
+			}
+			t.Eq(r, exp)
+		}
+}
+
 func TestEnv(t *testing.T) {
 	t.Parallel()
 	Run(&env{}, t)
+}
+
+type oserFX struct {
+	userHomeDir   func() (string, error)
+	getwd         func() (string, error)
+	userConfigDir func() (string, error)
+	mkdirAll      func(path string, _ fs.FileMode) error
+}
+
+func (x *oserFX) UserHomeDir() (string, error) {
+	if x.userHomeDir != nil {
+		return x.userHomeDir()
+	}
+	return os.UserHomeDir()
+}
+
+func (x *oserFX) Getwd() (string, error) {
+	if x.getwd != nil {
+		return x.getwd()
+	}
+	return os.Getwd()
+}
+
+func (x *oserFX) UserConfigDir() (string, error) {
+	if x.userConfigDir != nil {
+		return x.userConfigDir()
+	}
+	return os.UserConfigDir()
+}
+
+func (x *oserFX) MkdirAll(path string, fm fs.FileMode) error {
+	return os.MkdirAll(path, fm)
+}
+
+type fatalerMock struct {
+	fatal func(...interface{})
+}
+
+func (m *fatalerMock) Fatal(vv ...interface{}) {
+	m.fatal(vv...)
 }
